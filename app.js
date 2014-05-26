d53ad769aa3c478487f612309bd39db2c5f6b8fe
Smbat Yeranyan <smbat.yeranyan@gmail.com>
2014-05-26 22:24:19 +0200

    LeftRight only

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var engineControl = require("./lib/engineControl.js");
 
 
 directionParser.parser.on("backwards", function(value){
-  engineControl.engines.top1(value);
-  engineControl.engines.top2(value);
+/*  engineControl.engines.top1(value);
+  engineControl.engines.top2(value);*/
 });
 
 directionParser.parser.on("forward", function(value){
-  engineControl.engines.top1(value);
-  engineControl.engines.top2(value);
+/*  engineControl.engines.top1(value);
+  engineControl.engines.top2(value);*/
 });
 
 directionParser.parser.on("left", function(value){
